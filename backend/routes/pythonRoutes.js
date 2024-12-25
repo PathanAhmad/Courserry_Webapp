9edf953f25e6205350f732118eecbb315e8c7b29
Ahmad Pathan <pathanahmad2334@gmail.com>
2024-12-26 02:27:37 +0530

    Graph!!

--- backend/routes/pythonRoutes.js
+++ backend/routes/pythonRoutes.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const { processCSV } = require('../controllers/pythonController');
 
 // Route to fetch filtered graph data
-router.get('/process-csv', processCSV);
+router.get('/process-csv', (req, res) => {
+    console.log("CSV Route Triggered!");
+    processCSV(req, res);
+});
 
 module.exports = router;
